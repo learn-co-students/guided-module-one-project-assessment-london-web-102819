6e06bcbf4299ec9b2e049c41bee8f3a757d30e9e
domeazner <dominiccarolan@gmail.com>
2019-11-11 17:54:54 +0000

    got rid of pluralization

--- db/migrate/20191111173257_create_shows.rb
+++ db/migrate/20191111173257_create_shows.rb
@@ -1,9 +1,9 @@
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
-      t.string :shows_name
-      t.integer :episodes
-      t.integer :seasons
+      t.string :show_name
+      t.integer :episodes_watched
+      t.integer :seasons_completed
     end
   end
 end
