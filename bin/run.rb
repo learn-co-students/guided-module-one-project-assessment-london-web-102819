271dd28f1a31de4706d12bfd04bf15049f8ea048
PianoFF <Fangfei1star@gmail.com>
2019-11-13 10:22:25 +0000

    reseeded with log_in and password

--- bin/run.rb
+++ bin/run.rb
@@ -8,6 +8,18 @@ cli.new_or_not
 
 cli.your_field
 
+if cli.user_collaborator == true
+    cli.add_new_pianist
+elsif cli.user_pianist == true
+    cli.add_new_collaborator
+end
+
+cli.who_is_working 
+
+puts "Has it been another year? How time flies! Let's update your profile,pianist!
+        Collaborators please look the other way <--"
+cli.update_experience 
+
 
 
 
--- lib/commandlineinterface.rb
+++ lib/commandlineinterface.rb
@@ -1,7 +1,7 @@
 require_relative '../config/environment.rb'
 require "pp"
 class CommandLineInterface
-    attr_reader :new_in_house
+    attr_reader :new_in_house, :user_pianist, :user_collaborator
     def greet
         puts "Welcome to this fabulous fake opera house! Here, you'll find the best repertoire with the most bizzare instruments~ Shall we begin?" 
     end
@@ -9,12 +9,19 @@ class CommandLineInterface
     def new_or_not
         puts "Are you new to the House? Y/N"
         a = gets.chomp
-        if a.upcase == "Y"
-            @new_in_house=true
-            puts "Welcome and let me show you our in house artists."
-        else
-            @new_in_house=false 
-            puts "Welcome back! It must've been good last time~"
+        loop do 
+            if a.upcase == "Y"
+                puts "Welcome and let me show you our in house artists."
+                create_new_account
+                break
+            elsif a.upcase == "N"
+                puts "Welcome back! It must've been good last time~"
+                user_log_in
+                break 
+            else
+                greet
+                break    
+            end
         end
     end 
 
@@ -22,6 +29,7 @@ class CommandLineInterface
         puts "Are you a pianist or a collaborator(vocal or instrumental)? P/C"
         a=gets.chomp    
         if a.upcase=="P"
+            @user_pianist = true
             puts "Let me show you our current collaborators."
             instrumentalists = Collaborator.select{|each|each.instrument != nil}
             puts instrumentalists.map{|each|  "#{each.name} is an instrumentalist. Instrument:#{each.instrument}"}.sort 
@@ -29,6 +37,7 @@ class CommandLineInterface
             vocalists = Collaborator.select{|each|each.voice_type != nil}
             puts vocalists.map{|each|  "#{each.name} is an vocalist. Voice type: #{each.voice_type}"}.sort
         elsif a.upcase=="C"
+            @user_collaborator =true 
             puts "Here are the top 10 pianists in house with the most years of experiences. Take a look."
             Pianist.order(:years_of_experience).last(10).each{|each| 
                 puts "#{each.name}, years of experiences #{each.years_of_experience} in #{each.expertise} repertoire."
@@ -36,11 +45,70 @@ class CommandLineInterface
         end
     end
 
-    def display_pianists
-        puts "Would you like to take a look at our pianists roster? Y/N"
-        a = gets.chomp
-        if a.upcase == "Y"
-        
+    def add_new_pianist
+        puts "Did you see your pianist's name or should we invite him/her? invite/nvm"
+        a=gets.chomp
+        if a.upcase == "INVITE"
+            puts "May I have his/her full name?" 
+            new_pianist_name = gets.chomp
+            puts "What is his/her expertise(vocal/instrumental)?"
+            new_pianist_expertise = gets.chomp 
+            new_pianist = Pianist.create(name: new_pianist_name, expertise: new_pianist_expertise)
+            puts "There, we've taken care of inviting your pianist #{new_pianist.name}. Welcome to the house!"    
+        elsif a.upcase == "NVM"
+            puts "Have a good day!"
+        else 
+            puts "Try harder next time."
+        end 
+    end
+
+    def add_new_collaborator
+        puts "Did you see your collaborator's name or should we invite him/her? invite/nvm"
+        a=gets.chomp
+        if a.upcase == "INVITE"
+            puts "May I have his/her full name?" 
+            new_collaborator_name = gets.chomp
+            puts "Is he/her an instrumentalist or a vocalist? i/v"
+            new_collaborator_field = gets.chomp 
+            if new_collaborator_field.upcase == "I"
+                puts "What is his/her instrument?"
+                instrument= gets.chomp
+                new_collaborator = Collaborator.create(name: new_collaborator_name, instrument: instrument)
+                puts "Have fun making music with #{new_collaborator.name}!"
+            elsif  new_collaborator_field.upcase == "V"
+                puts "What is his/her voice type?"
+                voice_type= gets.chomp
+                new_collaborator = Collaborator.create(name: new_collaborator_name, voice_type: voice_type)
+                puts "Have fun making music with #{new_collaborator.name}!"
+            end   
+        elsif a.upcase == "NVM"
+            puts "Have a good day!"
+        else 
+            puts "Try harder next time."
+        end 
+    end
+
+    def who_is_working
+        puts "Which pianist's work load would you like to see? Please type his/her full name [Full Name]."
+        pianist=gets.chomp
+        if Pianist.find_by(name: pianist)==nil 
+            puts "I'm terribly sorry. But that pianist does not work for us yet."
+        else 
+            Pianist.find_by(name:pianist).collaborators.map{|each|
+            puts "#{pianist} is currently working with #{each.name} on #{each.instrumental_repertoire!=nil ? each.instrumental_repertoire : each.vocal_repertoire}"}
+        end 
+    end
+
+    def update_experience
+        puts "Congratulations! Yet another year~ Tell me your Full Name ^_^"
+        pianist_name = gets.chomp
+        pianist = Pianist.find_by(name:pianist_name)
+        if pianist !=nil 
+            pianist.years_of_experience +=1
+            pianist.save 
+            puts "Take care! And best of luck on your future journeys!"
+        else
+            puts "Ooops! This pianist does not appear to exist in our book. Until next time~"
         end
     end
 end
